1bddfba85ab5a96f7e690696e5ec8f316f83731b
nquanghien <nquanghien97@gmail.com>
2023-06-06 18:49:17 +0700

    implement filter product api

--- controllers/pagination.js
+++ controllers/pagination.js
@@ -3,8 +3,19 @@ import Product from '../models/products.js';
 export function paginationProduct (req, res, next) {
   const page = req.query.page || 1;
   const limit = req.query.limit || 5;
+  const {filter} = req.body
+  const listFilter = [
+    "gender",
+    "category",
+    "type"
+  ]
+  listFilter.forEach(element => {
+    if(filter[element] && filter[element].length === 0) {
+      delete filter[element]
+    }
+  });
 
-  Product.find()
+  Product.find(filter)
     .skip((limit * page) - limit)
     .limit(limit)
     .exec((_err, products) => {
